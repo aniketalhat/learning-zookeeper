024fe88e8bebca0ef6c88e216c3b219597cad37d
aniketalhat <aniket.alhat@gmail.com>
2014-12-29 14:24:39 +0900

    initial zk commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,13 @@
+/**
+ * Created by aniket on 12/26/14.
+ */
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+public class Client implements Watcher{
+    @Override
+    public void process(WatchedEvent event) {
+
+    }
+}
